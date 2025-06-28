1e1aee3710727e6685646f717974a52a63b69e93
KeiRuff <Keiruff07@gmail.com>
2025-06-28 09:36:02 -0400

    Update evo-calendar.js

--- js/evo-calendar.js
+++ js/evo-calendar.js
@@ -1,1 +1,48 @@
-
+$("#calendar").evoCalendar({
+  theme: "Orange Coral",
+  calendarEvents: [
+   {
+      id: "event1",
+      name: "Journey Toward Reparations Webinar",
+      date: "2025-06-24",
+      description: "7:00 PM ET<br>Online (Diocese of New Jersey)<br><a href='https://dioceseofnj.org/journey-toward-reparations/' target='_blank'>Details & Registration</a>",
+      type: "event"
+    },
+    {
+      id: "event2",
+      name: "Illinois ADCRC Full Commission Meeting",
+      date: "2025-06-28",
+      description: "Virtual + In-Person<br><a href="https://adcrc.illinois.gov/meetings.html">Meeting Details</a>",
+      type: "event"
+    },
+    {
+      id: "event3",
+      name: "ADCRC Full Commission Meeting",
+      date: "2025-07-18",
+      description: "Virtual + In-Person<br><a href="https://adcrc.illinois.gov/meetings.html">Meeting Details</a>",
+      type: "event"
+    },
+    {
+      id: "event4",
+      name: "ADCRC Full Commission Meeting",
+      date: "2025-08-15",
+      description: "Virtual + In-Person<br><a href="https://adcrc.illinois.gov/meetings.html">Meeting Details</a>",
+      type: "event"
+    },
+    {
+      id: "event5",
+      name: "The Big Payback Podcast",
+      date: ["2025-01-01", "2025-12-31"],
+      description: "New episodes every week<br><a href="https://thebigpayback.com/podcast">Listen & Subscribe</a>",
+      type: "event"
+    },
+    {
+      id: "event6",
+      name: "National Reparations Rally",
+      date: "2025-09-01",
+      description: "Washington, DC<br>Final Event Date TBA<br><a href="https://reparationscomm.org/">Check for Updates</a>",
+      type: "event"
+    },
+    
+  ]
+});
