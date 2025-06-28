09f75bd9677f5fc9800104e5dc0f22c955b93a83
KeiRuff <Keiruff07@gmail.com>
2025-06-28 10:37:42 -0400

    Update evo-calendar.js

--- js/evo-calendar.js
+++ js/evo-calendar.js
@@ -40,7 +40,7 @@ $("#calendar").evoCalendar({
       id: "event6",
       name: "National Reparations Rally",
       date: "2025-09-01",
-      description: "Washington, DC<br>Final Event Date TBA<br><a href='https://reparationscomm.org/' target='_blank'>Check for Updates</a>",
+      description: "Washington DC<br><a href='https://reparationscomm.org/' target='_blank'>Check for Updates</a>",
       type: "event"
     },
     
